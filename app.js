56f1bb3b4ab6bee8faf7094537cb6e1c3a6e7205
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2021-11-26 22:04:37 +0530

    sin up

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 await import("dotenv").then((dotenv) => dotenv.config());
+import mongoose from "mongoose";
 import { join, resolve } from "path";
 import { default as express } from "express";
+import bodyParser from "body-parser";
 const { HOST, PORT } = process.env;
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.set("views", join(resolve(), "/public"));
+const dblink = `mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/imgur?retryWrites=true&w=majority`;
+mongoose.connect(dblink).then(() => {
+  console.log("database connection succesfull");
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.post("/new", (req, res) => {
+  let { user } = req.body;
   res.render("profile", {
-    name: "amalu",
+    name: user,
   });
 });
 // server
